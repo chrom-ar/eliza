5fcc78d979aa248e07d6b800b07f0cca9a89d2cc
Batudo <82539708+batudo@users.noreply.github.com>
2025-03-09 14:57:02 -0300

    Simplified EVM swap + test fix (#45)

--- packages/plugin-chroma/src/solver/lifiEvmSwap.ts
+++ packages/plugin-chroma/src/solver/lifiEvmSwap.ts
@@ -4,57 +4,51 @@ import { getQuote } from '@lifi/sdk';
 import { getChainId, getTokenAmount, GeneralMessage, getTokenAddress } from './helpers';
 import { APPROVE_ABI } from './utils/abis';
 
-interface SwapBuilder {
-  buildSwapTransaction(message: GeneralMessage): Promise<{transaction: any}>;
-}
+export const buildSwapTransaction = async (message: GeneralMessage) => {
+  try {
+    const {
+      body: {
+        amount,
+        fromToken,
+        toToken,
+        fromAddress,
+        fromChain,
+      }
+    } = message;
 
-export class EVMLiFiSwap implements SwapBuilder {
-  async buildSwapTransaction(message: GeneralMessage) {
-    try {
-      const {
-        body: {
-          amount,
-          fromToken,
-          toToken,
-          fromAddress,
-          fromChain,
+    const fromChainId = getChainId(fromChain);
+    const fromTokenAmount = getTokenAmount(amount, fromChain, fromToken);
+
+    const quote = await getQuote({
+      fromChain: fromChainId,
+      toChain: fromChainId, // Same chain swap
+      fromToken: fromToken,
+      toToken: toToken,
+      fromAmount: fromTokenAmount,
+      fromAddress: fromAddress
+    });
+
+    const transactionRequest = quote.transactionRequest;
+
+    return {
+      transactions: [
+        {
+          chainId: fromChainId,
+          to: getTokenAddress(fromChain, fromToken),
+          value: 0,
+          data: encodeFunctionData({abi: APPROVE_ABI, functionName: "approve", args: [transactionRequest.to, fromTokenAmount]})
+        },
+        {
+          ...transactionRequest,
+          value: BigInt(transactionRequest.value).toString(),
+          gasPrice: BigInt(transactionRequest.gasPrice).toString(),
+          gasLimit: BigInt(transactionRequest.gasLimit).toString()
         }
-      } = message;
+      ]
+    };
 
-      const fromChainId = getChainId(fromChain);
-      const fromTokenAmount = getTokenAmount(amount, fromChain, fromToken);
-
-      const quote = await getQuote({
-        fromChain: fromChainId,
-        toChain: fromChainId, // Same chain swap
-        fromToken: fromToken,
-        toToken: toToken,
-        fromAmount: fromTokenAmount,
-        fromAddress: fromAddress
-      });
-
-      const transactionRequest = quote.transactionRequest;
-
-      return {
-        transactions: [
-          {
-            chainId: fromChainId,
-            to: getTokenAddress(fromChain, fromToken),
-            value: 0,
-            data: encodeFunctionData({abi: APPROVE_ABI, functionName: "approve", args: [transactionRequest.to, fromTokenAmount]})
-          },
-          {
-            ...transactionRequest,
-            value: BigInt(transactionRequest.value).toString(),
-            gasPrice: BigInt(transactionRequest.gasPrice).toString(),
-            gasLimit: BigInt(transactionRequest.gasLimit).toString()
-          }
-        ]
-      };
-
-    } catch (error) {
-      console.error("Error in buildSwapTransaction:", error);
-      throw error;
-    }
+  } catch (error) {
+    console.error("Error in buildSwapTransaction:", error);
+    throw error;
   }
 }
