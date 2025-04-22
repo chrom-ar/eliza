daaf7ee24d3a906c45fe75947153f10fc77a6156
Néstor Coppi <nestorcoppi@gmail.com>
2025-04-21 22:11:20 -0300

    Step 1

--- packages/plugin-chroma/src/services/solver.ts
+++ packages/plugin-chroma/src/services/solver.ts
@@ -1,18 +1,16 @@
 import { Service, ServiceType, IAgentRuntime, elizaLogger} from '@elizaos/core';
-import WClient from "@chrom-ar/waku-client";
+// import WClient from "@chrom-ar/waku-client";
 
-import { AVAILABLE_TYPES, buildResponse, signPayload } from '../solver';
+import SolverSDK from "@chrom-ar/solver-sdk";
+
+import { validateAndBuildProposal } from '../solver';
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export class SolverService extends Service {
   private initialized = false;
   private runtime: IAgentRuntime;
-  private interval: NodeJS.Timeout | null = null;
-  // @ts-ignore
-  private waku: any;
-
-  private config: object;
+  private solver: any;
 
   constructor() {
     super();
@@ -30,74 +28,7 @@ export class SolverService extends Service {
 
     this.runtime = runtime;
 
-    // TMP
-    this.config = {
-      PRIVATE_KEY: (() => {
-        const key = this.runtime.getSetting('SOLVER_PRIVATE_KEY');
-
-        if (!key) throw new Error('PRIVATE_KEY is not set in the environment variables.');
-
-        // If waku encryption is enabled for confidential messages,
-        // the private key must be the same, to keep correlation in staking
-        if (this.runtime.getSetting('WAKU_ENCRYPTION_PRIVATE_KEY') &&
-            this.runtime.getSetting('WAKU_ENCRYPTION_PRIVATE_KEY') != key) {
-          throw new Error('SOLVER_PRIVATE_KEY and WAKU_ENCRYPTION_PRIVATE_KEY MUST be the same.');
-        }
-
-        return key;
-      })()
-    }
-
-    this.waku = await WClient.start(this.runtime.getSetting.bind(this.runtime)); // WTF with the bind()
-    this.waku.setLogger(elizaLogger); // set the same logger
-
-    // Empty string for default topic
-    this.waku.subscribe('', async (event) => {
-      const response = await buildResponse(event, this.config);
-
-      if (!response) {
-        elizaLogger.info(`[SolverService] No response for ${event.replyTo}`);
-        return;
-      }
-
-      elizaLogger.info(`[SolverService] Sending response to ${event.replyTo}`, response);
-
-      await sleep(500); // Sleep a little time to wait for the chat
-      await this.waku.sendMessage(response, event.replyTo, event.replyTo);
-    });
-
-    // Handshake topic for confidential messages
-    this.waku.subscribe('handshake', async (event) => {
-      const { body: { type } } = event;
-
-      if (AVAILABLE_TYPES.includes(type?.toUpperCase())) {
-        elizaLogger.info(`[SolverService] Received ${type}-handshake for ${event.replyTo}`);
-      } else {
-        elizaLogger.info(`[SolverService] Received unknown ${type}-handshake for ${event.replyTo}`);
-        return
-      }
-
-      // Just send an ack to init communication
-      // @ts-ignore
-      const { signer, signature } = await signPayload({}, this.config);
-      const body = { signer, signature, signerPubKey: this.waku.publicKey };
-      await this.waku.sendMessage(body, event.body.replyTo, this.waku.publicKey);
-    });
-
-    this.waku.subscribe(this.waku.publicKey, async (event) => {
-      const response = await buildResponse(event, this.config);
-
-      if (!response) {
-        elizaLogger.info(`[SolverService] No response for confidential ${event.replyTo}`);
-        return;
-      }
-
-      elizaLogger.info(`[SolverService] Sending response to confidential ${event.replyTo}`);
-
-      await sleep(500); // Sleep a little time to wait for the chat
-      await this.waku.sendMessage(response, event.replyTo, this.waku.publicKey, event.body.signerPubKey);
-    }, { encrypted: true })
-
+    this.solver = await SolverSDK.start(validateAndBuildProposal, elizaLogger)
 
     elizaLogger.info('[SolverService] initialized');
 
